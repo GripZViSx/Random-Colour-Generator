43bf621dd5e5b87c0898ee3eaa18875716e27157
grozersniper38237@gmail.com <grozersniper38237@gmail.com>
2023-12-31 21:26:32 +0530

    Added some animation using JS and fixed text size

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ makecolor = function() {
  window.blue = Math.floor((Math.random() * 256) + 1);
  // Return RGB Code
  window.rgbcode = `rgb(${window.red}, ${window.green}, ${window.blue})`
- 
+
  // Return Hex Code
  hexcode = rgbToHex(red, green, blue);
  return hexcode;
@@ -49,12 +49,27 @@ reload.addEventListener("click", function() {
  reload.classList.toggle('rotate');
  window.refreshedcolor = makecolor()
  document.body.style.background = window.refreshedcolor;
+
+ code.style.opacity = 0;
+ redelement.style.opacity = 0;
+ blueelement.style.opacity = 0;
+ greenelement.style.opacity = 0;
+ rgbelement.style.opacity = 0;
+
  code.innerHTML = window.refreshedcolor;
  redelement.innerHTML = window.red;
  greenelement.innerHTML = window.green;
  blueelement.innerHTML = window.blue;
  rgbelement.innerHTML = window.rgbcode;
- 
+
+ setTimeout(() => {
+  code.style.opacity = 1;
+  redelement.style.opacity = 1;
+  blueelement.style.opacity = 1;
+  greenelement.style.opacity = 1;
+  rgbelement.style.opacity = 1;
+ }, 50)
+
  setTimeout(() => { reload.classList.toggle('rotate'); }, 500)
 })
 
@@ -76,24 +91,24 @@ greenelement.addEventListener("click", () => {
  navigator.clipboard.writeText(window.green.toString());
 })
 
-rgbelement.addEventListener("click", ()=> {
+rgbelement.addEventListener("click", () => {
  navigator.clipboard.writeText(window.rgbcode)
 })
 
 
 // Some Other CSS
-code.addEventListener("mousedown",()=>{
+code.addEventListener("mousedown", () => {
  code.style.color = window.rgbcode;
 })
 
-code.addEventListener("mouseup", ()=>{
+code.addEventListener("mouseup", () => {
  code.style.color = "#fff";
 })
 
-code.addEventListener("touchstart",  ()=>{
+code.addEventListener("touchstart", () => {
  code.style.color = window.rgbcode;
 })
 
-code.addEventListener("touchend", ()=>{
+code.addEventListener("touchend", () => {
  code.style.color = "#fff";
 })
